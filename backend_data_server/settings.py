8a641ced36a069abe48bda8090b616a177d7df70
emrubio85 <119766137+emrubio85@users.noreply.github.com>
2026-01-14 18:44:57 +0000

    GET Y POST guia 21

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'homepage',
+    "rest_framework",
+    'homepage',    
+    "demo_rest_api",
 ]
 
 MIDDLEWARE = [
